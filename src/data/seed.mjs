b0a49b4047b7885c81880981c81fa45d33d0e650
girination <bamegirry@gmail.com>
2025-07-29 10:41:04 +0200

    Removed API keys, added .env to .gitignore

--- src/data/seed.mjs
+++ src/data/seed.mjs
@@ -3,7 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 import { questions } from "./QuestionsData.js";
 
+<<<<<<< HEAD
 //  Firebase config
+=======
+>>>>>>> 2ba7cc5 (Updated to support database capturing of logged in users and record results)
 const firebaseConfig = {
   apiKey: "AIzaSyBhij5xFuhj7lcNs-OyEH4HKI_2CVS_rSk",
   authDomain: "aca-quizapp-f4dee.firebaseapp.com",
@@ -13,11 +16,17 @@ const firebaseConfig = {
   appId: "1:996349462838:web:02f6656c4fdf41374f4b97"
 };
 
+<<<<<<< HEAD
 //  Init Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 //  Seed function with duplicate check
+=======
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+>>>>>>> 2ba7cc5 (Updated to support database capturing of logged in users and record results)
 async function seedQuestions() {
   const colRef = collection(db, "questions");
   const existingSnapshot = await getDocs(colRef);
